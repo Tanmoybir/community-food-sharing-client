7c10795c133cbe627f6767882c406210e2fb4b23
Tanmoy Bir <tanmoy7468@gmail.com>
2024-02-27 00:35:10 +0600

    axios hook maked and add food form setup and tanstack query install

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { RouterProvider } from 'react-router-dom'
 import myCreateRoute from './route/myCreateRoute'
 import AuthProvider from './provider/AuthProvider'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-    <RouterProvider router={myCreateRoute}/>
-    <Toaster/>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={myCreateRoute} />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,12 +2,14 @@ import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndP
 import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import { auth } from "../firebase/firebase.config";
+import useAxios from "../hooks/useAxios";
 
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState('')
     const [loading, setLoading] = useState(true)
+    const axios = useAxios()
 
     const createUser = (email, password) => {
         setLoading(true)
@@ -20,11 +22,11 @@ const AuthProvider = ({ children }) => {
 
     }
 
-    const profile = (name,photo) => {
+    const profile = (name, photo) => {
         setLoading(true)
-       return updateProfile(auth.currentUser, {
+        return updateProfile(auth.currentUser, {
             displayName: name, photoURL: photo
-          })
+        })
     }
 
     const logOut = () => {
@@ -35,6 +37,14 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = () => {
             onAuthStateChanged(auth, currentUser => {
+                const loggedUser = { email: currentUser.email }
+                console.log(loggedUser);
+                if (currentUser) {
+                    axios.post('/user/access-token', loggedUser)
+                        .then(res => {
+                            console.log('Token', res.data);
+                        })
+                }
                 setUser(currentUser)
                 setLoading(false)
             })
@@ -42,9 +52,9 @@ const AuthProvider = ({ children }) => {
         return () => {
             unsubscribe()
         }
-    },[])
+    }, [axios])
 
-    
+
 
     const authInfo = {
         user,
